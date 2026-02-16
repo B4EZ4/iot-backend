d757bdaeb09c76680c21ffbc4fe0ee5226783488
Anton-Bazh <baezaantoniosacial01@gmail.com>
2026-02-15 22:18:15 -0600

    Actualizar constante en server.js

--- server.js
+++ server.js
@@ -92,8 +92,11 @@ app.post("/sensor-data", async (req, res) => {
    SERVIDOR
 ========================= */
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+/* =========================
+   SERVIDOR
+========================= */
